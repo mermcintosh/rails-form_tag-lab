92906d9143c2567b2fbeb7199b51a7506dee416d
Meredith <meredith@mcintosh.io>
2020-12-09 23:16:35 -0500

    commit tree 7a411a5594639552b6959207743e9bb0e5386676

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,15 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(first_name: params[:student][:first_name])
+    redirect_to students_path
+  end
+
+
+
 end
